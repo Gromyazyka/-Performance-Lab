b5c13a68e376747552549d35b6e54dbc95bdac89
Gromyazyka <111848239+Gromyazyka@users.noreply.github.com>
2024-06-29 10:29:11 +0300

    Add files via upload

--- task2/task2.py
+++ task2/task2.py
@@ -1,25 +1,26 @@
-def f_circle(x,y,x0,y0,R):
-    return (x-x0)**2 + (y-y0)**2 - R**2
+def where_dot(file_circle, file_dot):
 
-def where_dot(x,y,x0,y0,R):
-    if f_circle(x,y,x0,y0,R) == 0:
-        return 0
-    elif f_circle(x,y,x0,y0,R) > 0:
-        return 2
-    elif f_circle(x,y,x0,y0,R) < 0:
-        return 1
+    with open(file_circle, 'r', encoding='utf-8') as f1:
+        data1 = f1.readlines()  
 
-with open('file1.txt', 'r', encoding='utf-8') as f1:
-    data1 = f1.readlines()  
+    with open(file_dot, 'r', encoding='utf-8') as f2:
+        data2 = f2.readlines()
 
-with open('file2.txt', 'r', encoding='utf-8') as f2:
-    data2 = f2.readlines()
+    x0 = float(data1[0].split()[0])
+    y0 = float(data1[0].split()[1])
+    R = float(data1[1])
 
-x0 = float(data1[0].split()[0])
-y0 = float(data1[0].split()[1])
-R = float(data1[1])
+    for i in range(len(data2)):
+        x = float(data2[i].split()[0])
+        y = float(data2[i].split()[1])
+        
+        f = (x-x0)**2 + (y-y0)**2 - R**2
+    
+        if f == 0:
+            print(0)
+        elif f > 0:
+            print(2)
+        elif f < 0:
+            print(1)
 
-for i in range(len(data2)):
-    x = float(data2[i].split()[0])
-    y = float(data2[i].split()[1])
-    print(where_dot(x,y,x0,y0,R))
+#where_dot('file1.txt', 'file2.txt')
--- task4/task4.py
+++ task4/task4.py
@@ -1,17 +1,19 @@
-with open('numbers.txt', 'r', encoding='utf-8') as f:
-    nums = [int(line.strip()) for line in f]
+def get_min_count(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        nums = [int(line.strip()) for line in f]
+    
+    mean = int(sum(nums) / len(nums))
+    count = 0
 
-mean = int(sum(nums) / len(nums))
+    for a in nums:
+        while a != mean:
+            if a > mean:
+                a -= 1
+                count += 1
+            elif a < mean:
+                a += 1
+                count +=1
 
-count = 0
+    return count
 
-for a in nums:
-    while a != mean:
-        if a > mean:
-            a -= 1
-            count += 1
-        elif a < mean:
-            a += 1
-            count +=1
-            
-print(count)
+#print(get_min_count('numbers.txt'))
